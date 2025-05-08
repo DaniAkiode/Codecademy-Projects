f653e3e06b4382dc8dd7bb1c0250212b3954b6fa
DaniAkiode <dannyakiode@hotmail.co.uk>
2025-05-08 18:51:33 +0100

    New intro

--- PortfolioProjects/MentalHealth.py
+++ PortfolioProjects/MentalHealth.py
@@ -3,12 +3,17 @@ import time
 player_stats = {"Clarty": 5,
                 "Arsenal": []}
 
-
 def slow_print(text):
     for char in text: 
         print(char, end='', flush=True)
-        time.sleep(0.01)
+        time.sleep(0.07)
     print("\n")  
+
+def into():
+    slow_print("Welcome to the Mental Health Game!")
+    slow_print("In this game, you will be asked a series of questions to assess your mental health.")
+    slow_print("Please answer honestly to get the most accurate results.")
+    slow_print("Let's get started!")
 
 def anxietyLevel():
     pass
@@ -22,6 +27,9 @@ def stressLevel():
 def check_status():
     pass
 
-name = input("Please type your name")
+def main():
+    into()
 
 
+if __name__ == "__main__":
+    main()
