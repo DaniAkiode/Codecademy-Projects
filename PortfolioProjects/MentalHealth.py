af919e2627865511f18d5b11aa39a2ea3ccc9c7b
DaniAkiode <dannyakiode@hotmail.co.uk>
2025-05-08 17:13:59 +0100

    New code

---
+++ PortfolioProjects/MentalHealth.py
@@ -0,0 +1,27 @@
+import time 
+
+player_stats = {"Clarty": 5,
+                "Arsenal": []}
+
+
+def slow_print(text):
+    for char in text: 
+        print(char, end='', flush=True)
+        time.sleep(0.01)
+    print("\n")  
+
+def anxietyLevel():
+    pass
+
+def depresssionLevel():
+    pass
+
+def stressLevel():
+    pass
+
+def check_status():
+    pass
+
+name = input("Please type your name")
+
+
