a7c351ef47f272d8f1b7ceca71bedbe374097bf8
DaniAkiode <dannyakiode@hotmail.co.uk>
2025-12-13 15:31:42 +0000

    initialise supercalculator

---
+++ PortfolioProjects/Super-Calculator/supacalculator.py
@@ -0,0 +1,13 @@
+class Calculator:
+    def __init__(self):
+        pass
+    def add(self):
+        pass
+    def subtract(self):
+        pass
+    def multply(self):
+        pass
+    def divide(self):
+        pass
+    def modules(self):
+        pass
