f44f4b241ffebdaa1c5d7a2ef31984fbe73c2147
DaniAkiode <dannyakiode@hotmail.co.uk>
2025-12-15 18:20:22 +0000

    function on lock

--- PortfolioProjects/Super-Calculator/supacalculator.py
+++ PortfolioProjects/Super-Calculator/supacalculator.py
@@ -1,13 +1,44 @@
 class Calculator:
-    def __init__(self):
-        pass
-    def add(self):
-        pass
-    def subtract(self):
-        pass
-    def multply(self):
-        pass
-    def divide(self):
-        pass
-    def modules(self):
-        pass
+    def __init__(self, numbers):
+        self.numbers = numbers 
+
+    def add(self, numbers):
+        total = 0
+        for num in numbers:
+            total += num 
+        return total
+    
+    def subtract(self, numbers):        
+        if numbers:
+            total = numbers[0]
+            for num in numbers[1:]:
+                total -= num 
+        else:
+            total = 0
+        return total 
+
+    def multply(self, numbers):
+        total = 0
+        for num in numbers:
+            total *= num 
+        return total
+    
+    def divide(self, numbers):
+        if not numbers:
+            return 0
+        result = numbers[0]
+        for num in numbers[1:]:
+            if num == 0:
+                raise ValueError("Division By Zero")
+            result /= num
+        return result
+                    
+    def modulo(self, numbers):
+        if not numbers:
+            return 0     
+        result = numbers[0] 
+        for num in numbers[1:]:
+            if num == 0:
+                raise ValueError("Modulation by Zero")
+            result %= num
+        return result
